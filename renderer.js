52dd2ced93ec632bc60bb454e55045e814aa5679
HippoInWindow20 <shippo.hsu@gmail.com>
2022-05-23 13:16:24 +0800

    Add CSS

--- renderer.js
+++ renderer.js
@@ -3,33 +3,32 @@ const NOTIFICATION_BODY = 'Notification from the Renderer process. Click to log 
 const CLICK_MESSAGE = 'Notification clicked!'
 
 new Notification(NOTIFICATION_TITLE, { body: NOTIFICATION_BODY })
-  .onclick = () => document.getElementById("output").innerText = CLICK_MESSAGE
+    .onclick = () => document.getElementById("output").innerText = CLICK_MESSAGE
 
 
-function openNavbar () {
-  $( "#navbar" ).animate({
-    left: "0px",
-  }, 300, function() {
-  });
-  $( "#overlay" ).show().animate({
-    opacity: "1",
-  }, 300, function() {
-  });
+function openNavbar() {
+    $("#navbar").animate({
+        left: "0px",
+    }, 300, function() {});
+    $("#overlay").show().animate({
+        opacity: "1",
+    }, 300, function() {});
+    document.body.style.overflow = "hidden";
 }
 
-function closeNavbar () {
-  $( "#navbar" ).animate({
-    left: "-400px",
-  }, 300, function() {
-  });
-  $( "#overlay" ).animate({
-    opacity: "0",
-  }, 300, function() {
-  });
-  setTimeout(function() {
-    document.getElementById("overlay").style.display = "none";
-  },300);
+function closeNavbar() {
+    $("#navbar").animate({
+        left: "-400px",
+    }, 300, function() {});
+    $("#overlay").animate({
+        opacity: "0",
+    }, 300, function() {});
+    setTimeout(function() {
+        document.getElementById("overlay").style.display = "none";
+    }, 300);
+    document.body.style.overflow = "auto";
 }
 
-document.getElementById("menuicon").addEventListener("click",openNavbar);
-document.getElementById("overlay").addEventListener("click",closeNavbar);
+document.getElementById("menuicon").addEventListener("click", openNavbar);
+document.getElementById("overlay").addEventListener("click", closeNavbar);
+document.getElementById("closeBtn").addEventListener("click", closeNavbar);
